32476954ed874453a7222d913842fc6ea8e90e4c
i1SaSa <s9ny398335@gmail.com>
2025-10-15 16:43:40 +0000

    6

---
+++ hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+printf("Size of a char =%d\n",sizeof(char));
+printf("Size of an int =%d\n",sizeof(int));
+printf("Size of a long in =%d\n",sizeof(long));
+printf("Size of a long long int =%d\n",sizeof(double));
+printf("Size of a float =%d\n",sizeof(float));
+return (0);
+}
