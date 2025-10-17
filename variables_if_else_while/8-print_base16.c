3d631f256ab5630c28eec461cc7568e5f45e39ea
i1SaSa <s9ny398335@gmail.com>
2025-10-17 21:08:51 +0000

    8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,12 +7,12 @@
  **/
 int main(void)
 {
-  int x;
-  int y;
-  for (y = '0'; y <= '9'; ++y)
-    putchar(y);
-  for (x = 'a'; x <= 'f'; ++x)
-    putchar(x);
-  putchar('\n');
-  return (0);
+int x;
+int y;
+for (y = '0'; y <= '9'; ++y)
+putchar(y);
+for (x = 'a'; x <= 'f'; ++x)
+putchar(x);
+putchar('\n');
+return (0);
 }
