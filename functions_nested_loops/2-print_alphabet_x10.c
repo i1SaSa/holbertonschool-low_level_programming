b0ba730a460ed21f82f31fd3c04b0e308e32f249
i1SaSa <s9ny398335@gmail.com>
2025-10-19 15:34:48 +0000

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
-/** print_alphabet_x10 - print alphabet 10 times
- *
- *
+/**
+* print_alphabet_x10 - print alphabet 10 times
+*
+*
 */
 
 
@@ -10,15 +11,12 @@
 void print_alphabet_x10(void)
 {
 int itretion;
-for (itretion = 0; itretion <= 10; ++itretion)
+for (itretion = 0; itretion <= 9; ++itretion)
 {
 int letter;
 for (letter = 'a'; letter <= 'z'; ++letter)
 {
 _putchar(letter);
-
-
-  
 }
 _putchar('\n');
 }
