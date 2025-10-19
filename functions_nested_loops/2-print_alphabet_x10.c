8f38a15690dc4d000d240b1952ede93195174d76
i1SaSa <s9ny398335@gmail.com>
2025-10-19 15:31:20 +0000

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/** print_alphabet_x10 - print alphabet 10 times
+ *
+ *
+*/
+
+
+
+
+void print_alphabet_x10(void)
+{
+int itretion;
+for (itretion = 0; itretion <= 10; ++itretion)
+{
+int letter;
+for (letter = 'a'; letter <= 'z'; ++letter)
+{
+_putchar(letter);
+
+
+  
+}
+_putchar('\n');
+}
+
+}
