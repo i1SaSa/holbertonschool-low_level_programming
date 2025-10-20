7259903f92eb1eaea9dd5b2b12bd58a2fbacb60c
i1SaSa <s9ny398335@gmail.com>
2025-10-20 15:22:53 +0000

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_numbers - p
+*
+*
+*
+*/
+void print_numbers(void)
+{
+int c;
+for (c = '1'; c <= '9'; ++c)
+putchar (c);
+
+
+
+putchar ('\n');
+
+
+
+}
