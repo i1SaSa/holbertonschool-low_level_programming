7867fa9bc75ebd2dba4635575da22b32e5923b8a
i1SaSa <s9ny398335@gmail.com>
2025-10-21 13:02:50 +0000

    C

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,11 @@
 void print_most_numbers(void)
 {
 	int num;
-	for (num = '0'; num <= 9; ++num)
+	for (num = '0'; num <= '9'; ++num)
 	{
-		_putchar(num);
 		if (num == '2' || num == '4')
 			continue;
+		_putchar(num);
 	}
 
 
