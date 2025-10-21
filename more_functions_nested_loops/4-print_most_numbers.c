dea1d726d86ea3cecce0412024ddb6e8538a526b
i1SaSa <s9ny398335@gmail.com>
2025-10-21 12:59:02 +0000

    C

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_most_numbers - print numbers to 9 ecxept 2 & 4
+*
+*
+*
+*/
+void print_most_numbers(void)
+{
+	int num;
+	for (num = '0'; num <= 9; ++num)
+	{
+		_putchar(num);
+		if (num == '2' || num == '4')
+			continue;
+	}
+
+
+}
