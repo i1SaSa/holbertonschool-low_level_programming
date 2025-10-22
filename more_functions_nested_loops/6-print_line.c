449e06acd4a3c195beadfc30a81395c17f190af2
i1SaSa <s9ny398335@gmail.com>
2025-10-22 14:00:15 +0000

    6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,11 @@
 void print_line(int n)
 {
 	int count;
-	if (n <= 0)
-		_putchar('\n');
-	else
 		for (count = 0; count <= n; ++count)
 		{
+			if (n <= 0)
+				continue;
 			_putchar('_');
-			_putchar('\n');
 		}
+	_putchar('\n');
 }
