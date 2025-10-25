375cdba99705b95bb3a42385fbc6255c3bf59c38
i1SaSa <s9ny398335@gmail.com>
2025-10-25 16:36:13 +0000

    10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -11,7 +11,7 @@ void print_triangle(int size)
 	if (size > 0)
 	{
 
-		for (line = 0; line <= size; line++)
+		for (line = 1; line <= size; line++)
 		{
 			for (space = size - line; space > 0; space--)
                 		_putchar(' ');
