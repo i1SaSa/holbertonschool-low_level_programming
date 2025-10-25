b52cdf19d103aa59e45653b125a35959b5356b3e
i1SaSa <s9ny398335@gmail.com>
2025-10-25 11:35:17 +0000

    8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -11,7 +11,7 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (line = 0; line <= size; ++line)
+		for (line = 0; line < size; ++line)
 		{
 			for (count = 0; count < size; count++)
 				_putchar('#');
