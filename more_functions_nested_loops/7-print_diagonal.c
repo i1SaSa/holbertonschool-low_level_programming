c4a0643b59b3f8b50f3ebb87851a4a364a3525c7
i1SaSa <s9ny398335@gmail.com>
2025-10-25 10:01:27 +0000

    7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+* print_diagonal - draws a diagonal
+* @n: number of time '\' will be printed
+*
+*/
+
+
+void print_diagonal(int n)
+{
+	int line;
+	int space;
+
+	if (n > 0)
+	{
+		for (line = 0; line <= n; ++line)
+		{
+			_putchar('\n');
+			for (space = 0; space <= n; ++space)
+			{
+				_putchar(' ');
+			}
+			_putchar(n);
+		}
+
+		_putchar('\n');
+	} else
+		_putchar('\n');
+
+
+
+}
