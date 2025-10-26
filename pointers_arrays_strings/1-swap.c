f2c2330a58238a216bc01cc7b0c0e5a56a77577b
i1SaSa <s9ny398335@gmail.com>
2025-10-26 13:24:36 +0000

    1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* swap_int - swap to variable by pointers
+*
+* @a: the value will be swaped
+* @b: the value will be swaped
+*/
+
+void swap_int(int *a, int *b)
+{
+	*a = &b
+	*b = &a
+
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifdef main_h
 #define main_h
 void reset_to_98(int *n);
-
+void swap_int(int *a, int *b);
 
 
 
