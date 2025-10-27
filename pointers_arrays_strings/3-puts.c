1f13957ab97d12377a77e97f3be3cea800abb9c2
i1SaSa <s9ny398335@gmail.com>
2025-10-27 13:39:07 +0000

    3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 /**
+* _puts - print a string
 *
-*
-*
+*@str: the srting that will be printed
 *
 */
 void _puts(char *str)
 {
-	if (*str == '\0')
-		return;
-_puts(str);
+	while (*str)
+		_putchar(*str++);
 
-
+	_putchar('\n');
 }
