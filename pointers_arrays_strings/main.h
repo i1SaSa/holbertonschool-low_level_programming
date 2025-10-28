ae47135c15e92ae30aa173a33b00034fd8fc8185
i1SaSa <s9ny398335@gmail.com>
2025-10-28 12:35:40 +0000

    commit

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
-#ifdef main_h
-#define main_h
+#ifdef MAIN_H
+#define MAIN_H
 #include <stdlib.h>
-void reset_to_98(int *n);
 int _putchar(char c);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 size_t _strlen(const char *str);
 void _puts(char *str);
