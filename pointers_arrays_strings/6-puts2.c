ab9ba5b01cab79dd3b847352cdc4da127da67a4b
i1SaSa <s9ny398335@gmail.com>
2025-10-29 15:55:05 +0000

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,25 @@
 #include "main.h"
 /**
-*
-*
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+/**
+* puts2 - print other char in the string
+* @str: the string
 *
 */
 void puts2(char *str)
 {
-	int i = 0;
+	int index = 0;
 
-
+	while (str[index] != '\0')
+	{
+		++index;
+		if (index % 2 != 0)
+			_putchar(str[index]);
+	}
 }
