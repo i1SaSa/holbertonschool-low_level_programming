5818fcd05b19d44d66c972c69a95fd954f80e7f3
i1SaSa <s9ny398335@gmail.com>
2025-11-02 14:58:43 +0000

    0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -8,7 +8,7 @@
  *
  * Return: A pointer to the filled memory area @s.
  */
-void *_memset(void *s, int c, size_t n)
+char *_memset(void *s, int c, size_t n)
 {
 	unsigned int index;
 	unsigned char *memory = s, value = c;
