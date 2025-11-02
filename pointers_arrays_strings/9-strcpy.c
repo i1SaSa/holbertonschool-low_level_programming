295fa6f0e5f0ef9acf889be69b72bcec9b0c050c
i1SaSa <s9ny398335@gmail.com>
2025-11-02 09:39:03 +0000

    9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,16 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+    int index = 0;
 
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
+    while (src[index])
+    {
+        dest[index] = src[index];
+        index++;
+    }
 
-	return (dest);
+    dest[index] = '\0';
+
+    return (dest);
 }
+
