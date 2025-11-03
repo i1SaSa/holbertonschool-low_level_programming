40da5cd05709c7081b8f8d368e13aff69ceb3b4e
i0SaSa <s9ny398335@gmail.com>
2025-11-03 13:55:28 +0000

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print number of argument in the compile processe
+ *
+ *@argc: number of argument
+ *@argv: arry of the argument
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
