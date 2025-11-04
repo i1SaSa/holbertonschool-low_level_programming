5f2de9ff616e209336451293f6fdb447d0072bd2
i1SaSa <s9ny398335@gmail.com>
2025-11-04 13:07:12 +0000

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -15,12 +15,12 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
 
-	multi = atoi(argv[1]) * atoi(argv [2]);
-	printf("%d\n", multi);	
+	multi = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", multi);
 
 	return (0);
 }
