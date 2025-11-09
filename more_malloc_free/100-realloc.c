d66a5737ed4627d113f62fcc455a6faaa9eaf158
i1SaSa <s9ny398335@gmail.com>
2025-11-09 18:58:31 +0000

    commit

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -15,4 +15,32 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
+		return (new_ptr);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+
+	old_ptr = ptr;
+
+	for (i = 0; i < old_size && i < new_size; i++)
+		new_ptr[i] = old_ptr[i];
+
+	free(ptr);
+	return (new_ptr);
+}
 
