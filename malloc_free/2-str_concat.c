8f8f9b822f276c5ff55edf054d8182f8db1c603b
i1SaSa <s9ny398335@gmail.com>
2025-11-09 14:58:07 +0000

    1

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include "main.h"
+
 /**
  * str_concat - concatenates two strings
  * @s1: first string
@@ -10,30 +10,31 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *newstr;
-    unsigned int i, j, len1 = 0, len2 = 0;
+	char *newstr;
+	unsigned int i, j, len1 = 0, len2 = 0;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    while (s1[len1] != '\0')
-        len1++;
-    while (s2[len2] != '\0')
-        len2++;
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
-    newstr = malloc(sizeof(char) * (len1 + len2 + 1));
-    if (newstr == NULL)
-        return (NULL);
+	newstr = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (newstr == NULL)
+		return (NULL);
 
-    for (i = 0; i < len1; i++)
-        newstr[i] = s1[i];
+	for (i = 0; i < len1; i++)
+		newstr[i] = s1[i];
 
-    for (j = 0; j < len2; j++, i++)
-        newstr[i] = s2[j];
+	for (j = 0; j < len2; j++, i++)
+		newstr[i] = s2[j];
 
-    newstr[i] = '\0';
+	newstr[i] = '\0';
 
-    return (newstr);
+	return (newstr);
 }
+
