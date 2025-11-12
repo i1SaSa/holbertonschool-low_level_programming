32bde13e8b2273d7619f6fcd1185bbf706d6f90b
i0SaSa <s9ny398335@gmail.com>
2025-11-12 15:19:43 +0000

    0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_name - print a name that recived from user
+ *@*name: the name will be printed
+ *@*f: the way of printing the name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+ if (name && f)
+        f(name);
+}
