f4fa92511fe4cdb12eb9324d4a9deb2a9674208f
i1SaSa <s9ny398335@gmail.com>
2025-11-24 15:17:30 +0000

    ADD: Function that print List element

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		count++;
+		h = h->next;
+	}
+
+
+}
