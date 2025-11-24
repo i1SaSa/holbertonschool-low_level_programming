09f37b8459ce26e44bbe7330ff71ed198bc602a0
i1SaSa <s9ny398335@gmail.com>
2025-11-24 15:26:55 +0000

    Fix: betty stile

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -7,7 +7,7 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 
@@ -15,5 +15,5 @@ size_t print_list(const list_t *h)
 		h = h->next;
 	}
 
-return count;
+return (count);
 }
