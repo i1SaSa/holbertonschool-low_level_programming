63a9324da6bdd66d1d5c4768d9f05e8de3640544
i1SaSa <s9ny398335@gmail.com>
2025-11-25 14:04:20 +0000

    Fix return and line sequance

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,18 +12,17 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 	int count = 0;
 
-	new->str = strdup(str);
-
-	while(str[count])
-		++count;
-
-	new->len = count;
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
 
+	new->str = strdup(str);
 
+        while(str[count])
+                ++count;
+
+        new->len = count;
 	new->next = *head;
-	head = *new;
-	return (head);
+	*head = new;
+	return (new);
 }
