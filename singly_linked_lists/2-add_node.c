2666279220cf7362169136edc24d56d99dc84b6c
i1SaSa <s9ny398335@gmail.com>
2025-11-25 12:23:41 +0000

    Add: function that insert node at the beginnig

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * add_node - add node in the beginnig of the list
+ * @head: head of the first node
+ * @str:content of the node
+ *
+ * Return: if succes addres of the new node,
+ *	if faild return Null
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+
+	if (!new)
+		return (NULL);
+
+	new->next = *head;
+
+	return (*head);
+}
