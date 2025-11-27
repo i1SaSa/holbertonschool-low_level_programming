a8bd4337097abbf7eadbd51b9a772e47b1c492b2
i1SaSa <s9ny398335@gmail.com>
2025-11-27 16:24:00 +0300

    Fix memory leak in free_list function by freeing string before node

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -13,6 +13,7 @@ void free_list(list_t *head)
     while (current != NULL)
     {
         next_node = current->next;
+        free(current->str);
         free(current);
         current = next_node;
     }
