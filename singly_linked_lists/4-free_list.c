161ec352b1f6e897a8f3bc68e60d4cafd68628cd
i1SaSa <s9ny398335@gmail.com>
2025-11-27 16:19:13 +0300

    ADD free list function

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *  free_list - free the list from the memory
+ * @head: the current node
+ * 
+ *
+ */
+void free_list(list_t *head)
+{
+    list_t *current, *next_node;
+    
+    current = head;
+    while (current != NULL)
+    {
+        next_node = current->next;
+        free(current);
+        current = next_node;
+    }
+    
+}
