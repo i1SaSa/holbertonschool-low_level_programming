916f0b444ceaa30e2e8560ae5ccf1bd90bd834a7
i1SaSa <s9ny398335@gmail.com>
2025-11-27 17:33:30 +0300

    Add print_dlistint function and header file for doubly linked list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+size_t print_dlistint(const dlistint_t *h)
+{
+    const dlistint_t *current = h;
+    unsigned int count = 0;
+
+    if (!h)
+    {
+        return (0);
+    }
+    while (current->prev != NULL)
+    current = current->prev;
+
+
+    while (current != NULL)
+    {
+        printf("[%u]\n", current->n);
+        current = current->next;
+        ++count;
+    }
+    
+}
---
+++ doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+in this section we will continue exploring doubly linked lists.
