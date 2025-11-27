3efde66dab859686fd1ba09ec5e4a54c6ca0c84e
i1SaSa <s9ny398335@gmail.com>
2025-11-27 15:53:03 +0300

    Refactor comments in add_node_end function for clarity and consistency

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,38 +1,38 @@
 #include "lists.h"
 /**
  * add_node_end - adds a node at the end of a list_t list
- * @head: pointer to the pointer of the first node
- * @str: string to store in the new node
+ * @head: pointer to pointer to the first node
+ * @str: string to duplicate into the node
  *
  * Return: address of the new element, or NULL
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new, *curr;
-    unsigned int len = 0;
+	list_t *new, *curr;
+	unsigned int len = 0;
 
-    new = malloc(sizeof(list_t));
-    if (!new)
-        return (NULL);
-    new->str = strdup(str);
-    if (!new->str)
-    {
-        free(new);
-        return (NULL);
-    }
-    while (str[len])
-        len++;
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	new->str = strdup(str);
+	if (!new->str)
+	{
+		free(new);
+		return (NULL);
+	}
+	while (str[len])
+		len++;
+	new->len = len;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	curr = *head;
+	while (curr->next)
+		curr = curr->next;
 
-    new->len = len;
-    new->next = NULL;
-    if (*head == NULL)
-    {
-        *head = new;
-        return (new);
-    }
-    curr = *head;
-    while (curr->next)
-        curr = curr->next;
-    curr->next = new;
-    return (new);
+	curr->next = new;
+	return (new);
 }
