be5df193cd53042894b2ede897d3d43e8f9bbff7
i1SaSa <s9ny398335@gmail.com>
2025-11-29 15:23:46 +0300

    Fix to kernal style

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,22 +1,28 @@
 #include "lists.h"
+/**
+ * print_dlistint - print every element in doubly linkedlist
+ * @h: pointer to node
+ * 
+ * Return: count of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *current = h;
-    size_t count = 0;
+	const dlistint_t *current = h;
+	size_t count = 0;
 
-    if (!h)
-    {
-        return (0);
-    }
-    while (current->prev != NULL)
-    current = current->prev;
+	if (!h) {
+		return (0);
+	}
 
+	/* Move backward to the head */
+	while (current->prev != NULL)
+		current = current->prev;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-        ++count;
-    }
-    return (count);
+	/* Print list forward */
+	while (current != NULL) {
+		printf("%d\n", current->n);
+		current = current->next;
+		count++;
+	}
+	return (count);
 }
