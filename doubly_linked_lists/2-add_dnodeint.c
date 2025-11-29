ad1fd7f28219c932ed36e952f76b368cd6ea648e
i1SaSa <s9ny398335@gmail.com>
2025-11-29 19:32:35 +0300

    Fix formatting and improve documentation in add_dnodeint function

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,21 +1,24 @@
 #include "lists.h"
 /**
-*
-*
-*
-*/
+ * add_dnodeint - add node at the start of the doubly linkedlist
+ * @head: pointer to the first node
+ * @n:	
+ *
+ * Return: in succes first node address,
+ * 	if failed NULL
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
+	int number;
 
-	new = head;
+	new = malloc(sizeof(dlistint_t));
+
 	if (!new)
 		return (NULL);
-
-	while (new->prev != NULL)
-	{
-		new = new->prev;
-	}
+	new->next = *head;
+	*head = new;
+	new->n = number;
 
 	return (new);
 }
