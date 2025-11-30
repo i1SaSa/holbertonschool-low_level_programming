07412abd29d93df9716536dc47af01ee5c2e4211
i1SaSa <s9ny398335@gmail.com>
2025-11-30 16:14:05 +0300

    Fix logic error in free_dlistint function to correctly traverse the list

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -10,7 +10,7 @@ void free_dlistint(dlistint_t *head)
 	dlistint_t *current, *next_node;
 
 	current = head;
-	while (current->next != NULL)
+	while (current->next == NULL)
 	{
 		next_node = current->next;
 		free(current);
