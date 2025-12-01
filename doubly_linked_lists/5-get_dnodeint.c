c91a1ed7e92dd91a629be96526ff8d136bf2df80
i1SaSa <s9ny398335@gmail.com>
2025-12-01 19:40:58 +0300

    Fix documentation for get_dnodeint_at_index function

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,8 +1,10 @@
 #include "lists.h"
 /**
- *	get_dnodeint_at_index - 
+ *	get_dnodeint_at_index - returns the nth node of a dlistint_t linked list.
+ *	@head: pointer to the first node
+ *	@index: is the index of the node
  *
- *
+ * Return: Null of the node doesn't exist
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
