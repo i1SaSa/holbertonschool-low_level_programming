713e62940470806a5ffd9f2694b13654e173e0f1
i1SaSa <s9ny398335@gmail.com>
2025-12-07 23:31:36 +0300

    Add function documentation for read_textfile

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters it should read and print.
+ *
+ * Return: The actual number of letters it could read and print.
+ * 0 if the file cannot be opened or read, if filename is NULL,
+ * or if write fails or does not write the expected amount of bytes.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
