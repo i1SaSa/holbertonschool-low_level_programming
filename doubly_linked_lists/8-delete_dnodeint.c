3b739601bf8eb3b30fb8bec6b2d7c11b1a24d238
i1SaSa <s9ny398335@gmail.com>
2025-12-07 16:43:07 +0300

    Remove unused variable next_node in delete_dnodeint_at_index function

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -9,7 +9,7 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
-	dlistint_t *current, *next_node;
+	dlistint_t *current;
 
 	if (!(*head))
 		return (-1);
