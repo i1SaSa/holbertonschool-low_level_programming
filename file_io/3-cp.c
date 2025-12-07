e6685a75b88fe67010d7382cfe7fabb605a4cac6
i1SaSa <s9ny398335@gmail.com>
2025-12-07 23:39:36 +0300

    Add 3-cp.c to implement file copy functionality with error handling

---
+++ file_io/3-cp.c
@@ -0,0 +1,81 @@
+#include "main.h"
+
+/**
+ * error_check - checks if file descriptors can be closed.
+ * @fd: The file descriptor to close.
+ *
+ * Return: void
+ */
+void error_check(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUF_SIZE];
+	char *file_from, *file_to;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_from = argv[1];
+	file_to = argv[2];
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		error_check(fd_from); 
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+
+		if (bytes_written == -1 || bytes_written != bytes_read)
+		{
+			error_check(fd_from);
+			error_check(fd_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		error_check(fd_from);
+		error_check(fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	error_check(fd_from);
+	error_check(fd_to);
+
+	return (0);
+}
