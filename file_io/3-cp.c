3fa48a90e79f6b665bf9551344f35dc1e47cc52c
i1SaSa <s9ny398335@gmail.com>
2025-12-07 23:45:42 +0300

    Refactor whitespace in 3-cp.c for improved readability

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -28,18 +28,15 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		error_exit(97, "Usage: cp file_from file_to\n", "");
-
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		close(fd_from);
 		error_exit(99, "Error: Can't write to %s\n", argv[2]);
 	}
-
 	while ((read_count = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		write_count = write(fd_to, buffer, read_count);
@@ -50,19 +47,15 @@ int main(int argc, char *argv[])
 			error_exit(99, "Error: Can't write to %s\n", argv[2]);
 		}
 	}
-
 	if (read_count == -1)
 	{
 		close(fd_from);
 		close(fd_to);
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 	}
-
 	if (close(fd_from) == -1)
 		error_exit(100, "Error: Can't close fd %d\n", argv[1]);
-
 	if (close(fd_to) == -1)
 		error_exit(100, "Error: Can't close fd %d\n", argv[2]);
-
 	return (0);
 }
